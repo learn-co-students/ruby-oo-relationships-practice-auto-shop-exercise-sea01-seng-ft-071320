78fcc87815e622db950e8063d458c729a1df6529
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-07-19 22:35:43 -0700

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,32 @@
+require 'pry'
+
 class Car
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
+  @@all= []
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+    @@all << self
   end
 
+  def self.all
+  @@all
+  end
+
+
+  def self.classifications
+    self.all.map {|car| car.classification}
+  end
+  
+  def self.find_mechanics(classification)
+    Mechanic.all.select do |mechanic| 
+      mechanic.specialty == classification
+    end
+  end
 end
+
