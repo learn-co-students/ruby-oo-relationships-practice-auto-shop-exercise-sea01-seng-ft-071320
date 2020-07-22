340817f38da3300ae012e9c996c93711f95d935a
afteralec <github email address>
2020-07-21 22:43:11 -0700

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,29 @@
 class Car
-
   attr_reader :make, :model
+  attr_accessor :car_owner, :mechanic
+
+  @@all = []
 
   def initialize(make, model, classification)
-    @make = make
-    @model = model
-    @classification = classification
+    @make, @model, @classification = make, model, classification
+    
+    self.save
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Class methods to aggregate data
+  def self.classifications
+    all.map(&:classification)
+  end
+
+  def self.find_mechanic(specialty)
+    Mechanic.all.select { |mechanic| mechanic.specialty == specialty }
+  end
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,37 @@
 class CarOwner
-
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+
+    self.save
   end
 
+  def self.all
+    @@all
+  end
 
+  def save
+    self.class.all << self
+  end
+
+  #Instance methods to fetch data
+  def cars
+    Car.all.select{ |car| car.car_owner == self }
+  end
+
+  def mechanics
+    cars.map(&:mechanic)
+  end
+
+  #Class methods to aggregate data
+  def self.total_number_of_cars
+    all.map(&:cars).flatten.length
+  end
+
+  def self.average_number_of_cars
+    (total_number_of_cars.to_f / all.length).round(1)
+  end
 end
